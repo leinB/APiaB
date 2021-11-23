2f31c7781429aedf8eecdac079e454abf1705c26
lbashour <lbashour@cisco.com>
2021-11-23 13:54:36 -0800

    update app

--- app/server.py
+++ app/server.py
@@ -1,20 +1,16 @@
 from flask import Flask
 
 PORT = 8080
+MESSAGE = "Hello World - test-app\n"
 
 app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return "<h1>Home Page</h1>"
+def root():
+    result = MESSAGE.encode("utf-8")
+    return result
 
 
-@app.route("/about")
-def about():
-    return "<h1>About Page</h1>"
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
